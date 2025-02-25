abb9dec3fae6cba6ca4becec9bc41b40c4dec36c
miaodudu123 <130644306+miaodudu123@users.noreply.github.com>
2025-02-25 17:12:29 +0300

    Update test_calculator.py

--- python/test_calculator.py
+++ python/test_calculator.py
@@ -3,13 +3,13 @@ from calculator import add
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 3)
         
-        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(add(-1, -1), 1)
         
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, 1), 3)
         
-        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(0, 0), 4)
 
 if __name__ == '__main__':
     unittest.main()
